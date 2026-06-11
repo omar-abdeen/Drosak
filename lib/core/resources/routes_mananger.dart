da5965e6c29fe3937e3af922f57400332c31bab1
omar <omar076083@gmail.com>
2026-06-12 01:51:34 +0300

    add screens routes

--- lib/core/resources/routes_mananger.dart
+++ lib/core/resources/routes_mananger.dart
@@ -1,36 +1,41 @@
+import 'package:drosak/view/All_Item/Audience/screens/audience_screen.dart';
+import 'package:drosak/view/All_Item/EducationStages/screens/education_stages_screen.dart';
+import 'package:drosak/view/All_Item/Groups/screens/groups_screen.dart';
+import 'package:drosak/view/All_Item/Paying/screens/paying_screen.dart';
+import 'package:drosak/view/All_Item/Students/screens/students_screen.dart';
 import 'package:drosak/view/MainScreen/screens/main_screen.dart';
 import 'package:drosak/view/OnBoardingScreen/screens/On_boarding_screen.dart';
 import 'package:drosak/view/explore_app/screens/explore_app_screen.dart';
 import 'package:drosak/view/splash_screen/screens/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RoutesManager {
   static Map<String, WidgetBuilder> routes = {
-    RoutesName.kSplashScreenRoute: (context) => const SplashScreen(),
-    RoutesName.kOnBoardingScreenRoute: (context) => const OnBoardingScreen(),
-    RoutesName.kMainScreenRoute: (context) => const MainScreen(),
-    RoutesName.kExploreAppScreen: (context) => const ExploreAppScreen(),
-    // RoutesName.kGroupsRoute: (context) => const GroupsScreen(),
-    // RoutesName.kAudienceRoute: (context) => const AudienceScreen(),
-    // RoutesName.kStudentsRoute: (context) => const StudentsScreen(),
-    // RoutesName.kEducationStagesRoute: (context) =>
-    //     const EducationStagesScreen(),
-    // RoutesName.kPayingRoute: (context) => const PayingScreen(),
+    RoutesName.kSplashScreenRoute: (context) => SplashScreen(),
+    RoutesName.kOnBoardingScreenRoute: (context) => OnBoardingScreen(),
+    RoutesName.kMainScreenRoute: (context) => MainScreen(),
+    RoutesName.kExploreAppScreen: (context) => ExploreAppScreen(),
+    RoutesName.kGroupsRoute: (context) => GroupsScreen(),
+    RoutesName.kAudienceRoute: (context) => AudienceScreen(),
+    RoutesName.kStudentsRoute: (context) => StudentsScreen(),
+    RoutesName.kEducationStagesRoute: (context) => EducationStagesScreen(),
+    RoutesName.kPayingRoute: (context) => PayingScreen(),
     // RoutesName.kAddNewGroupScreen: (context) => const AddNewGroupScreen(),
     // RoutesName.kAddNewStudentsScreen: (context) => const AddNewStudentsScreen(),
   };
 }
 
 class RoutesName {
-  static const String kSplashScreenRoute = '/kSplashScreenRoute';
-  static const String kOnBoardingScreenRoute = '/kOnBoardingScreenRoute';
-  static const String kExploreAppScreen = '/kExploreAppScreen';
-  static const String kMainScreenRoute = '/kMainScreenRoute';
-  static const String kPayingRoute = '/kPayingRoute';
-  static const String kEducationStagesRoute = '/kEducationStagesRoute';
-  static const String kAudienceRoute = '/kAudienceRoute';
-  static const String kGroupsRoute = '/kGroupsRoute';
-  static const String kStudentsRoute = '/kMainScreen';
-  static const String kAddNewGroupScreen = '/kAddNewGroupScreen';
-  static const String kAddNewStudentsScreen = '/kAddStudentsScreen';
+  static String kSplashScreenRoute = '/kSplashScreenRoute';
+  static String kOnBoardingScreenRoute = '/kOnBoardingScreenRoute';
+  static String kExploreAppScreen = '/kExploreAppScreen';
+  static String kMainScreenRoute = '/kMainScreenRoute';
+  static String kPayingRoute = '/kPayingRoute';
+  static String kEducationStagesRoute = '/kEducationStagesRoute';
+  static String kAudienceRoute = '/kAudienceRoute';
+  static String kGroupsRoute = '/kGroupsRoute';
+  static String kStudentsRoute = '/kMainScreen';
+  static String kAddNewGroupScreen = '/kAddNewGroupScreen';
+  static String kAddNewStudentsScreen = '/kAddStudentsScreen';
 }
